4eed6b698da482700b32a65e245dc2e0fc36c588
havelka <havelka@HAVELKA>
2023-01-12 12:11:33 +0100

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldControler.cs
@@ -0,0 +1,36 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldControler : Controller
+    {
+        private static List<DogViewModul> dogs = new List<DogViewModul>();
+        public IActionResult Index()
+        {
+            DogViewModul doggo = new DogViewModul();
+
+
+            return View(dogs);
+        }
+
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModul();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModul dogViewModul)
+        {
+            dogs.Add(dogViewModul);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who is there?";
+        }
+
+    }
+}
---
+++ FirstMVCApp/Models/DogViewModul.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace FirstMVCApp.Models
+{
+    public class DogViewModul
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
